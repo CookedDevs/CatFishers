758b4170d902e59caba3e4a34089a02cbf65f074
RealDoggoHitman <lahtinenvili1@gmail.com>
2025-09-04 16:33:00 +0300

    Added join menu

--- Client/src/Menu.cpp
+++ Client/src/Menu.cpp
@@ -3,78 +3,208 @@
 #include <raylib.h>
 #include <math.h>
 #include <functional>
+#include <vector>
 
 #define NUM_FRAMES  3 
+#define NUM_FRAMES_TEXTFIELD  4
 
 const int screenWidth = 800;
 const int screenHeight = 450;
 
 static Camera2D camera;
 static float timeValue = 0.0f;
-Texture2D NewGameButton;
-Rectangle btnBounds;
+static Vector2 mousePoint = { 0.0f, 0.0f };
 
-int btnState = 0;               
-bool NewGameAction = false;         
+static Button newGameBtn;
+static Button joinGameBtn;
+static Button settingsBtn;
+static Button playBtn;
+static Button backBtn;
+static Textfield serverIpTxtField;
+static Textfield playerNamepTxtField;
 
-Color color = WHITE;
+Font TextFont;
 
-Vector2 mousePoint = { 0.0f, 0.0f };
 
-float frameHeight;
-Rectangle sourceRec;
+static std::vector<Button*> buttons;
+static std::vector<Textfield*> textfields;
 
 void Menu::UnInit()
 {
-    UnloadTexture(NewGameButton);
+    for (auto b : buttons)UnloadTexture(b->texture);
+    for (auto b : textfields)UnloadTexture(b->texture);
 }
 
 std::string Menu::GetName() { return "Menu"; }
+
+static void InitButton(Button &btn, const char* path, Vector2 pos, float scale, bool enabled) {
+    btn.texture = LoadTexture(path);
+    btn.frameHeight = (float)btn.texture.height / NUM_FRAMES;
+    pos.x -= btn.texture.width * scale / 2.0f;
+    pos.y -= btn.frameHeight * scale / 2.0f;
+    btn.sourceRec = { 0, 0, (float)btn.texture.width, btn.frameHeight };
+    btn.bounds = { pos.x, pos.y, btn.texture.width * scale, btn.frameHeight * scale };
+    btn.enabled = enabled;
+    btn.state = 0;
+    buttons.push_back(&btn);
+}
+static void InitTextfield(Textfield &txtfld, const char* path, Vector2 pos, float scale, bool enabled) {
+    txtfld.texture = LoadTexture(path);
+    txtfld.frameHeight = (float)txtfld.texture.height / NUM_FRAMES_TEXTFIELD;
+    pos.x -= txtfld.texture.width * scale / 2.0f;
+    pos.y -= txtfld.frameHeight * scale / 2.0f;
+    txtfld.sourceRec = { 0, 0, (float)txtfld.texture.width, txtfld.frameHeight };
+    txtfld.bounds = { pos.x, pos.y, txtfld.texture.width * scale, txtfld.frameHeight * scale };
+    txtfld.enabled = enabled;
+    txtfld.state = 0;
+    textfields.push_back(&txtfld);
+}
+
+static void UpdateButton(Button &btn) {
+    mousePoint = GetMousePosition();
+
+    if (CheckCollisionPointRec(mousePoint, btn.bounds)) {
+        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) btn.state = 2;
+        else btn.state = 1;
+
+        if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT) && btn.onClick) {
+            btn.onClick();
+        }
+    }
+    else btn.state = 0;
+
+    btn.sourceRec.y = btn.state * btn.frameHeight;
+}
+static void UpdateTextfield(Textfield &txtfld) {
+    mousePoint = GetMousePosition();
+
+    bool hovered = CheckCollisionPointRec(mousePoint, txtfld.bounds);
+    if (hovered && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+        txtfld.focused = true;
+    } else if (!hovered && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+        txtfld.focused = false;
+    }
+
+    if (txtfld.focused) {
+        int key = GetCharPressed();
+        while (key > 0) {
+            if (key >= 32 && key <= 125) {
+                txtfld.text.push_back((char)key);
+            }
+            key = GetCharPressed();
+        }
+        if (IsKeyPressed(KEY_BACKSPACE) && !txtfld.text.empty()) {
+            txtfld.text.pop_back();
+        }
+        if (IsKeyPressed(KEY_ENTER)) {
+            txtfld.focused = false;
+        }
+    }
+
+    if (txtfld.focused) {
+        txtfld.state = 3;
+    } else if (hovered && txtfld.text.empty()) {
+        txtfld.state = 1;
+    } else if (!txtfld.text.empty()) {
+        txtfld.state = 2;
+    } else {
+        txtfld.state = 0;
+    }
+
+    txtfld.sourceRec.y = txtfld.state * txtfld.frameHeight;
+}
+
+
+static void DrawButton(const Button &btn) {
+    DrawTexturePro(btn.texture, btn.sourceRec, btn.bounds, Vector2{0, 0}, 0.0f, WHITE);
+}
+static void DrawTextfield(const Textfield &txtfld) {
+    DrawTexturePro(txtfld.texture, txtfld.sourceRec, txtfld.bounds, Vector2{0, 0}, 0.0f, WHITE);
+
+    float maxFontSize = 40;
+    float minFontSize = 10;
+    float spacing = 1.0f;
+    float fontSize = maxFontSize;
+
+    std::string displayText = txtfld.text;
+
+
+    while (fontSize > minFontSize &&
+           MeasureTextEx(TextFont, displayText.c_str(), fontSize, spacing).x > txtfld.bounds.width - 10) {
+        fontSize -= 1.0f;
+    }
+
+    if (displayText.empty()) fontSize = maxFontSize;
+
+    Vector2 textSize = MeasureTextEx(TextFont, displayText.empty() ? "_" : displayText.c_str(), fontSize, spacing);
+    Vector2 textPos = {
+        txtfld.bounds.x + (txtfld.bounds.width - textSize.x) / 2.0f,
+        txtfld.bounds.y + (txtfld.bounds.height - textSize.y) / 2.0f
+    };
+
+    if (!displayText.empty()) {
+        DrawTextEx(TextFont, displayText.c_str(), textPos, fontSize, spacing, Color{ 131, 144, 176, 255 });
+    }
+    else if (txtfld.focused) {
+        DrawTextEx(TextFont, "_", textPos, fontSize, spacing, Color{ 131, 144, 176, 255 });
+    }
+}
+
+
+
 void Menu::Init()
 {
-    camera.target = Vector2{ 0.0f, 0.0f};
-    NewGameButton = LoadTexture("Resources/Images/newgame.png");
-    NewGameButton.height = NewGameButton.height/2;
-    NewGameButton.width = NewGameButton.width/2;
+    camera.target = Vector2{ 0.0f, 0.0f };
 
-    frameHeight = (float)NewGameButton.height/NUM_FRAMES;
-    sourceRec = { 0, 0, (float)NewGameButton.width, frameHeight};
-
-    btnBounds = { screenWidth/2.0f - NewGameButton.width/2.0f, (screenHeight/2.0f - NewGameButton.height/NUM_FRAMES/2.0f) - 50, (float)NewGameButton.width, frameHeight };
+    TextFont = LoadFontEx("Resources/fredoka-one.ttf", 100, nullptr, 0);
 
 
+    InitButton(newGameBtn,  "Resources/Images/newgame.png",  { screenWidth/2.0f, screenHeight/2.0f - 110 }, 0.5f , true);
+    InitButton(joinGameBtn, "Resources/Images/joingame.png", { screenWidth/2.0f, screenHeight/2.0f - 50  }, 0.5f , true);
+    InitButton(settingsBtn, "Resources/Images/settings.png", { screenWidth/2.0f, screenHeight/2.0f + 10  }, 0.5f , true);
+    InitButton(playBtn, "Resources/Images/play.png", { screenWidth/2.0f - 130, screenHeight/2.0f - 50 }, 0.5f , false);
+    InitButton(backBtn, "Resources/Images/back.png", { screenWidth/2.0f - 145, screenHeight/2.0f + 10 }, 0.5f , false);
+
+    InitTextfield(serverIpTxtField, "Resources/Images/serverip.png", { screenWidth/2.0f + 40, screenHeight/2.0f + 10 }, 0.5f , false);
+    InitTextfield(playerNamepTxtField, "Resources/Images/playername.png", { screenWidth/2.0f + 57, screenHeight/2.0f - 50 }, 0.5f , false);
+
+    newGameBtn.onClick = []() { std::cout << "Starting new game...\n"; };
+    joinGameBtn.onClick = []() {
+        playBtn.enabled = true;
+        backBtn.enabled = true;
+        playerNamepTxtField.enabled = true;
+        serverIpTxtField.enabled = true;
+        settingsBtn.enabled = false;
+        joinGameBtn.enabled = false;
+        newGameBtn.enabled = false;
+    };
+    settingsBtn.onClick = []() { std::cout << "Opening settings...\n"; };
+    playBtn.onClick = []() { std::cout << "Playing..\n"; };
+    backBtn.onClick = []() { 
+        serverIpTxtField.text = playerNamepTxtField.text = "";        
+        playBtn.enabled = false;
+        backBtn.enabled = false;
+        playerNamepTxtField.enabled = false;
+        serverIpTxtField.enabled = false;
+        settingsBtn.enabled = true;
+        joinGameBtn.enabled = true;
+        newGameBtn.enabled = true;
+
+    };
 }
 
 void Menu::Update()
 {
-    mousePoint = GetMousePosition();
     timeValue += GetFrameTime();
-    NewGameAction = false;
-
-    if (CheckCollisionPointRec(mousePoint, btnBounds))
-    {
-        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) btnState = 2;
-        else btnState = 1;
-
-        if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) NewGameAction = true;
-    }
-    else btnState = 0;
-
-    if (NewGameAction)
-    {
-        // TODO: Any desired action
-    }
-
-    sourceRec.y = btnState*frameHeight;
+    for (auto* b : buttons) if (b->enabled) UpdateButton(*b);
+    for (auto* t : textfields) if (t->enabled) UpdateTextfield(*t);
 
     BeginDrawing();
         ClearBackground(SKYBLUE);
-        DrawTextureRec(NewGameButton, sourceRec, Vector2{ btnBounds.x, btnBounds.y}, WHITE);
-
+        for (auto* b : buttons) if (b->enabled) DrawButton(*b);
+        for (auto* t : textfields) if (t->enabled) DrawTextfield(*t);
         BeginMode2D(camera);
         EndMode2D();
-
         DrawFPS(10, 10);
     EndDrawing();
 }
-
--- Client/src/Menu.h
+++ Client/src/Menu.h
@@ -1,20 +1,41 @@
 #pragma once
 #include "State.h"
+#include <raylib.h>
+#include <string>
+#include <functional>
+
+struct Button {
+    Texture2D texture;
+    Rectangle bounds;
+    Rectangle sourceRec;
+    float frameHeight;
+    int state;
+    bool enabled;
+    std::function<void()> onClick;
+};
+
+struct Textfield {
+    Texture2D texture;
+    Rectangle bounds;
+    Rectangle sourceRec;
+    float frameHeight;
+    int state;
+    bool focused = false;
+    bool enabled = true;
+    std::string text;
+};
+
 
 class Menu : public State
 {
 public:
-	Menu() {}
-	~Menu() {}
+    Menu() {}
+    ~Menu() {}
 
-	void UnInit() override;
-	void Init() override;
-	void Update() override;
-	std::string GetName() override;
+    void UnInit() override;
+    void Init() override;
+    void Update() override;
+    std::string GetName() override;
+
 private:
-	int perlinNoiseMapLoc;
-	int timeLoc;
 };
-
-
-
