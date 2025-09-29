48c5e2a36551b99cc82544d4f95e19fccef6e1e1
Saltzus <sasu.lahtinen@outlook.com>
2025-09-29 20:07:50 +0300

    Saving system working on phone too

--- Client/src/ClientConfig.cpp
+++ Client/src/ClientConfig.cpp
@@ -1,39 +1,49 @@
 #include "ClientConfig.h"
 
+#ifdef _ANDROID_
+std::string path = "config.json";
+#else // _ANDROID_
+std::string path = "Resources/config.json";
+#endif
+
+#ifdef _ANDROID_
+#include <raymob.h>
+#include <android/log.h>
+#define LOGI(fmt, ...) __android_log_print(ANDROID_LOG_INFO, "ClientConfig", fmt, ##__VA_ARGS__)
+#define LOGE(fmt, ...) __android_log_print(ANDROID_LOG_ERROR, "ClientConfig", fmt, ##__VA_ARGS__)
+#else
+#include <raylib.h>
+#define LOGI(fmt, ...) printf(fmt "\n", ##__VA_ARGS__)
+#define LOGE(fmt, ...) fprintf(stderr, fmt "\n", ##__VA_ARGS__)
+#endif // _ANDROID_
 
 void ClientConfig::Init()
 {
-    std::ifstream ifFile("Resources/config.json");
-    if (ifFile)
+    char* text = LoadFileText(path.c_str());
+    if (text)
     {
-        data = nlohmann::json::parse(ifFile);
+        LOGE("whaat it found?");
+        LOGE("%s", text);
+        data = nlohmann::json::parse(text);
         name = data["name"].get<std::string>();
         ip = data["ip"].get<std::string>();
         UUID = data["UUID"].get<std::string>();
-        ifFile.close();
+        UnloadFileText(text);
+        return;
     }
-    else
-    {
-        data["name"] = name;
-        data["ip"] = ip;
-        data["UUID"] = UUID;
 
-        std::ofstream ofFile("Resources/config.json");
-        ofFile << data.dump(4);
-        ofFile.close();
-    }
+    Save();
 }
 
 void ClientConfig::Save()
 {
-    std::ofstream file("Resources/config.json");
     data["name"] = name;
     data["ip"] = ip;
     data["UUID"] = UUID;
 
-    if (file.good() && file.is_open())
-    {
-        file << data.dump(4);
-        file.close();
-    }
+    bool ok = SaveFileText(path.c_str(), data.dump(4).c_str());
+    if (ok) LOGE("OK");
+    else LOGE("NOTOK");
 }
+
+
